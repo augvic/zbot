aebc80a0aeab036af693852bada7c1e7f836e21a
AugustoViceli <a.viceli7357@gmail.com>
2025-09-30 17:00:32 -0300

    Changes in structure.

---
+++ src/storage/web_client/pages/zindex.ts
@@ -0,0 +1,298 @@
+export default class zIndex {
+    
+    element!: HTMLElement
+    menu!: Menu
+    titleBar!: TitleBar
+    module!: Module
+    
+    constructor() {
+        this.createSelf();
+        this.createComponents();
+        document.body.appendChild(this.element);
+    }
+    
+    private createSelf() {
+        this.element = document.createElement("div");
+        this.element.className = "h-full w-full overflow-hidden flex flex-col bg-gray-300 dark:bg-gray-900 transition-colors duration-300 opacity-fade-in";
+    }
+    
+    private createComponents() {
+        this.module = new Module(this.element);
+        this.menu = new Menu(this.element);
+        this.titleBar = new TitleBar(this.element);
+    }
+    
+}
+
+class TitleBar {
+    
+    element!: HTMLElement
+    menuButton!: MenuButton
+    themeButton!: ThemeButton
+    
+    constructor(appendTo: HTMLElement) {
+        this.createSelf();
+        this.createComponents();
+        appendTo.appendChild(this.element);
+    }
+    
+    private createSelf() {
+        this.element = document.createElement("div");
+        this.element.id = "title_bar";
+        this.element.className = "w-full bg-white dark:bg-gray-700 fixed z-50 h-[50px] flex items-center pl-3 transition-colors duration-300";
+    }
+    
+    private createComponents() {
+        this.menuButton = new MenuButton(this.element);
+        this.themeButton = new ThemeButton(this.element);
+    }
+    
+}
+
+class MenuButton {
+    
+    element!: HTMLElement
+    icon!: Icon
+    
+    constructor(appendTo: HTMLElement) {
+        this.createSelf();
+        this.createComponents();
+        this.startListeners();
+        appendTo.appendChild(this.element);
+    }
+    
+    private createSelf() {
+        this.element = document.createElement("button");
+        this.element.id = "menu_button";
+        this.element.className = "cursor-pointer w-auto h-auto rounded-md transition-colors duration-300 hover:bg-gray-300 dark:hover:bg-gray-900";
+    }
+    
+    private createComponents() {
+        if (document.documentElement.classList.contains("light")) {
+            this.icon = new Icon(this.element, "menu-icon", "/static/images/menu_light.png");
+        } else {
+            this.icon = new Icon(this.element, "menu-icon", "/static/images/menu_dark.png");
+        }
+    }
+    
+    private startListeners() {
+        this.element.addEventListener("click", () => {
+            const menu = document.getElementById("menu")!;
+            if (menu.style.display == "none") {
+                menu.classList.add("fade-in-left");
+                menu.style.display = "flex";
+                menu.addEventListener("animationend", () => {
+                    menu.classList.remove("fade-in-left");
+                }, { once: true });
+            } else {
+                menu.classList.add("fade-out-left");
+                menu.addEventListener("animationend", () => {
+                    menu.classList.remove("fade-out-left");
+                    menu.style.display = "none";
+                }, { once: true });
+            }
+        });
+    }
+    
+}
+
+class Icon {
+    
+    element!: HTMLImageElement
+    
+    constructor(appendTo: HTMLElement, id: string, src: string) {
+        this.createSelf(src, id);
+        appendTo.appendChild(this.element);
+    }
+    
+    private createSelf(src: string, id: string) {
+        this.element = document.createElement("img");
+        this.element.src = src;
+        this.element.id = id;
+        this.element.className = "size-7 opacity-fade-in";
+    }
+    
+}
+
+class Menu {
+    
+    element!: HTMLElement
+    
+    constructor(appendTo: HTMLElement) {
+        this.createSelf();
+        this.createComponents();
+        appendTo.appendChild(this.element);
+    }
+    
+    private createSelf() {
+        this.element = document.createElement("div");
+        this.element.id = "menu";
+        this.element.className = "w-[300px] flex-col fixed bg-white/30 dark:bg-gray-700/30 z-40 p-3 gap-y-3 bottom-0 rounded-br-lg rounded-tr-lg";
+        this.element.style.height = "calc(100vh - 50px)";
+        this.element.style.backdropFilter = "blur(6px)";
+        this.element.style.display = "none";
+    }
+    
+    private async createComponents() {
+        const response = await fetch(`${window.location.origin}/session-modules`);
+        const modulesAllowed = await response.json();
+        if (modulesAllowed == null) {
+            return
+        }
+        modulesAllowed.forEach((module: { [key: string]: string; }) => {
+            new ModuleButton(this.element, module);
+        });   
+    }
+    
+}
+
+class ModuleButton {
+    
+    element!: HTMLElement
+    button!: HTMLElement
+    hoverSpan!: HoverSpan
+    
+    constructor(appendTo: HTMLElement, module: {[key: string]: string}) {
+        this.createSelf(module.module);
+        this.startListeners(module.module, module.description);
+        appendTo.appendChild(this.element);
+    }
+    
+    private createSelf(moduleName: string) {
+        this.element = document.createElement("div");
+        this.element.className = "flex w-auto h-auto items-center justify-center";
+        this.button = document.createElement("button");
+        this.button.className = "w-full h-auto p-2 bg-blue-700 hover:bg-blue-900 cursor-pointer rounded-md text-white transition-colors duration-300";
+        this.button.innerText = moduleName;
+        this.element.appendChild(this.button);
+    }
+    
+    private startListeners(moduleName:string, moduleDescription: string) {
+        this.button.addEventListener("click", () => {
+            const moduleContainer = document.getElementById("module")!;
+            const menu = document.getElementById("menu")!;
+            this.button.dispatchEvent(new Event("mouseleave"));
+            moduleContainer.classList.add("opacity-fade-out");
+            moduleContainer.addEventListener("animationend", async () => {
+                moduleContainer.classList.remove("opacity-fade-out");
+                moduleContainer.innerHTML = "";
+                const bundle = await import(`${window.location.origin}/module-bundle/${moduleName.toLowerCase()}`);
+                const bundleClass = bundle.default;
+                new bundleClass(moduleContainer);
+                menu.classList.add("fade-out-left");
+                menu.addEventListener("animationend", () => {
+                    menu.classList.remove("fade-out-left");
+                    menu.style.display = "none";
+                }, { once: true });
+            }, { once: true });
+        });
+        this.button.addEventListener("mouseenter", () => {
+            let hoverSpan = new HoverSpan(this.element, moduleDescription);
+            this.button.addEventListener("mouseleave", () => {
+                hoverSpan.element.classList.remove("opacity-fade-in");
+                hoverSpan.element.classList.add("opacity-fade-out");
+                hoverSpan.element.addEventListener("animationend", () => {
+                    hoverSpan.element.remove();
+                }, { once: true });
+            }, { once: true });
+        });
+    }
+    
+}
+
+class Module {
+    
+    element!: HTMLElement
+    
+    constructor(appendTo: HTMLElement) {
+        this.createSelf();
+        appendTo.appendChild(this.element);
+    }
+    
+    private createSelf() {
+        this.element = document.createElement("div");
+        this.element.id = "module";
+        this.element.className = "w-full bg-gray-300 dark:bg-gray-900 transition-colors duration-300 justify-center items-center bottom-0 flex fixed cursor-default";
+        this.element.style.height = "calc(100vh - 50px)";
+        this.element.innerHTML = "<p class='text-black dark:text-white transition-colors duration-300'>✨ Selecione um módulo para começar.</p>"
+    }
+    
+}
+
+class HoverSpan {
+    
+    element!: HTMLElement
+    
+    constructor(appendTo: HTMLElement, text: string) {
+        this.createSelf(text);
+        appendTo.appendChild(this.element);
+    }
+    
+    private createSelf(text: string) {
+        this.element = document.createElement("span");
+        this.element.className = "text-nowrap opacity-fade-in z-50 bg-blue-900 text-white py-1 px-4 rounded-lg absolute ml-130 w-auto h-auto cursor-default";
+        this.element.style.fontSize = "small";
+        this.element.innerText = text;
+    }
+    
+}
+
+class ThemeButton {
+    
+    element!: HTMLElement
+    icon!: Icon
+    
+    constructor(appendTo: HTMLElement) {
+        this.createSelf();
+        this.createComponents();
+        this.startListeners();
+        appendTo.appendChild(this.element);
+    }
+    
+    private createSelf() {
+        this.element = document.createElement("button");
+        this.element.className = "cursor-pointer w-auto h-auto rounded-md transition-colors duration-300 hover:bg-gray-300 dark:hover:bg-gray-900 absolute right-3";
+    }
+    
+    private createComponents() {
+        let iconSrc = "";
+        if (window.localStorage.getItem("theme") == "light") {
+            iconSrc = "/static/images/moon.png";
+        } else {
+            iconSrc = "/static/images/sun.png";
+        }
+        this.icon = new Icon(this.element, "theme-button", iconSrc);
+    }
+    
+    private startListeners() {
+        this.element.addEventListener("click", () => {
+            const menuIcon = document.getElementById("menu-icon")! as HTMLImageElement;
+            if (document.documentElement.classList.contains("light")) {
+                document.documentElement.classList.remove("light");
+                document.documentElement.classList.add("dark");
+                window.localStorage.setItem("theme", "dark");
+                this.icon.element.src = "/static/images/sun.png";
+                this.icon.element.classList.remove("opacity-fade-in");
+                void this.icon.element.offsetWidth;
+                this.icon.element.classList.add("opacity-fade-in");
+                menuIcon.src = "/static/images/menu_dark.png";
+                menuIcon.classList.remove("opacity-fade-in");
+                void menuIcon.offsetWidth;
+                menuIcon.classList.add("opacity-fade-in");
+            } else {
+                document.documentElement.classList.remove("dark");
+                document.documentElement.classList.add("light");
+                window.localStorage.setItem("theme", "light");
+                this.icon.element.src = "/static/images/moon.png";
+                this.icon.element.classList.remove("opacity-fade-in");
+                void this.icon.element.offsetWidth;
+                this.icon.element.classList.add("opacity-fade-in");
+                menuIcon.src = "/static/images/menu_light.png";
+                menuIcon.classList.remove("opacity-fade-in");
+                void menuIcon.offsetWidth;
+                menuIcon.classList.add("opacity-fade-in");
+            }
+        });
+    }
+    
+}
---
+++ src/tasks/validate_login.py
@@ -0,0 +1,29 @@
+from src.infrastructure.drivers.databases.production.clients import UsersClient, PermissionsClient, ModulesClient
+from src.infrastructure.managers import SessionManager
+
+class ValidateLogin:
+    
+    def _setup(self) -> None:
+        self.users_client = UsersClient()
+        self.session_manager = SessionManager()
+        self.permissions_client = PermissionsClient()
+        self.modules_client = ModulesClient()
+    
+    def execute(self, login_data: dict) -> dict:
+        self._setup()
+        user = self.users_client.read(login_data["user"])
+        if user == None:
+            return {"success": False}
+        if user.password != login_data["password"]:
+            return {"success": False}
+        self.session_manager.save_in_session("user", user.user)
+        modules = self.modules_client.read("all")
+        modules_descriptions = {}
+        for module in modules:
+            modules_descriptions[module.module] = module.description
+        user_permissions = self.permissions_client.read(user.user)
+        permissions_list = []
+        for user_permission in user_permissions:
+            permissions_list.append({"module": user_permission.module, "description": modules_descriptions[user_permission.module]})
+        self.session_manager.save_in_session("session_modules", permissions_list)
+        return {"success": True}
